36fe6abd3d9dffffd89b6d2d736fe24d3ea65328
lsm <lsm1998.aliyun.com>
2020-06-12 17:08:07 +0800

    改进服务端消息回显

--- client/handler/listeners.go
+++ client/handler/listeners.go
@@ -7,7 +7,10 @@ import (
 	"net"
 	"os"
 	"strings"
+	"time"
 )
+
+var resultChannel = make(chan string, 1)
 
 var send = func(conn net.Conn) {
 	termReader := bufio.NewReader(os.Stdin)
@@ -19,10 +22,14 @@ var send = func(conn net.Conn) {
 		}
 		cmd := string(line)
 		cmd = strings.ReplaceAll(cmd, "\r\n", "")
+		cmd = strings.ReplaceAll(cmd, "\n", "")
 		switch cmd {
+		case "?":
+			printHelp()
 		case "pag":
 			transferFile()
 			sendShell("shell sh build.sh", conn)
+			printResult()
 		case "exit":
 			_ = conn.Close()
 			os.Exit(0)
@@ -35,10 +42,28 @@ var send = func(conn net.Conn) {
 				sendFile(conn)
 			} else if strings.Index(cmd, "shell") >= 0 {
 				sendShell(cmd, conn)
+				printResult()
 			} else {
-				fmt.Println("不能识别的命令")
+				fmt.Printf("不能识别的命令，你输入的是[%s]，可以输入'?'查看手册\n", cmd)
 			}
 		}
+	}
+}
+
+func printHelp() {
+	fmt.Println("pag 发包指令")
+	fmt.Println("shell [cmd] 执行命令")
+	fmt.Println("exit 退出程序")
+	fmt.Println("clear 清空命令行")
+	fmt.Println("upload [fileName] 上传文件")
+}
+
+func printResult() {
+	select {
+	case str := <-resultChannel:
+		fmt.Println("收到回复 ->\n", str)
+	case <-time.After(10 * time.Second):
+		fmt.Println("回复超时了！")
 	}
 }
 
@@ -64,11 +89,9 @@ func Listeners() {
 			}
 			result := buffer.String()
 			if strings.Index(result, "notify") >= 0 {
-				result = result[6:]
-				fmt.Println("收到消息->" + result)
-				BlockNotify()
+				resultChannel <- result[6:]
 			} else {
-				fmt.Println("收到消息->" + result)
+				resultChannel <- result
 			}
 		}
 	}
